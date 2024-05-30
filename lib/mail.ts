2b0a587250ec943cf41f059d5c4dea8a9de8b767
Milly <liuhongmein@gmail.com>
2024-05-30 18:03:09 -0400

    change hard coded dmian to env

--- lib/mail.ts
+++ lib/mail.ts
@@ -2,6 +2,8 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
+const domain = process.env.NEXT_PUBLIC_APP_URL;
+
 export async function sendTwoFactorTokenEmail(email: string, token: string) {
   await resend.emails.send({
     from: "Acme <onboarding@resend.dev>",
@@ -12,7 +14,7 @@ export async function sendTwoFactorTokenEmail(email: string, token: string) {
 }
 
 export async function sendVerificationEmail(email: string, token: string) {
-  const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
+  const confirmLink = `${domain}/auth/new-verification?token=${token}`;
   await resend.emails.send({
     from: "Acme <onboarding@resend.dev>",
     to: email,
@@ -22,7 +24,7 @@ export async function sendVerificationEmail(email: string, token: string) {
 }
 
 export async function sendPasswordResetEmail(email: string, token: string) {
-  const resetLink = `http://localhost:3000/auth/new-password?token=${token}`;
+  const resetLink = `${domain}/auth/new-password?token=${token}`;
 
   await resend.emails.send({
     from: "Acme <onboarding@resend.dev>",
